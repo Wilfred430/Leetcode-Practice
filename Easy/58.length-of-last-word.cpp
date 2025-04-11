24ccb7483dc3135dc8879477b403339e420fee27
WilfredEE <wendeyang2@gmail.com>
2025-04-11 16:12:29 +0800

    新增題目

--- Easy/58.length-of-last-word.cpp
+++ Easy/58.length-of-last-word.cpp
@@ -8,7 +8,10 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
+        stringstream ss(s);
+        string tmp;
+        while(ss >> tmp){}
+        return tmp.length();
     }
 };
 // @lc code=end
