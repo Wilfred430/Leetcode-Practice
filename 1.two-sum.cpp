92e1ffb337ab6057c8ff619c29c61f05815b0d6a
WilfredEE <wendeyang2@gmail.com>
2025-04-13 12:58:20 +0800

    新增變更

---
+++ 1.two-sum.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> ans;
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            if (ans.find(target - nums[i]) != ans.end()) 
+            {
+                return {ans[target - nums[i]], i};
+            }
+            ans[nums[i]] = i;
+        }
+        return {};
+    }
+};
+// @lc code=end
