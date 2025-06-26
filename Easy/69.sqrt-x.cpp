b03eefbd274be6ccbaea315c4f334abe4533ff8f
WilfredEE <wendeyang2@gmail.com>
2025-06-26 12:27:45 +0800

    add easy No.69

---
+++ Easy/69.sqrt-x.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=69 lang=cpp
+ *
+ * [69] Sqrt(x)
+ */
+#include<iostream>
+#include<cmath>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int mySqrt(int x) {
+        return sqrt(x);
+    }
+};
+// @lc code=end
+
