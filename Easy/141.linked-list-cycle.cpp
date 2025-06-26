968938feff4cb25bef8296226719a2108d16e03d
WilfredEE <wendeyang2@gmail.com>
2025-06-26 12:25:54 +0800

    add easy No.141

--- Easy/141.linked-list-cycle.cpp
+++ Easy/141.linked-list-cycle.cpp
@@ -7,6 +7,8 @@
 // @lc code=start
 
 // Definition for singly-linked list.
+#include<iostream>
+using namespace std;
 struct ListNode
 {
     int val;
@@ -19,6 +21,15 @@ class Solution
 public:
     bool hasCycle(ListNode *head)
     {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow) return true;
+        }
+        return false;
     }
 };
 // @lc code=end
