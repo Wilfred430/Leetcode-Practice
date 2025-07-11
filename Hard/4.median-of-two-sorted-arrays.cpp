b9a3cd43b2c98f461f2b35c1e33dafd21db60f16
WilfredEE <wendeyang2@gmail.com>
2025-07-11 15:22:02 +0800

    add No.4

--- Hard/4.median-of-two-sorted-arrays.cpp
+++ Hard/4.median-of-two-sorted-arrays.cpp
@@ -5,14 +5,38 @@
  */
 
 // @lc code=start
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-class Solution {
+
+class Solution
+{
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    {
+        vector<int> result;
+        for (int i = 0; i < nums1.size(); ++i)
+        {
+            result.push_back(nums1[i]);
+        }
+        for (int i = 0; i < nums2.size(); ++i)
+        {
+            result.push_back(nums2[i]);
+        }
+
+        sort(result.begin(), result.end());
+
+        if (result.size() % 2 == 1)
+        {
+            return double(result[result.size() / 2]);
+        }
+        else
+        {
+            return double(result[result.size() / 2] + result[result.size() / 2 - 1])/2.0;
+        }
+
+        return 0.0;
     }
 };
 // @lc code=end
-
