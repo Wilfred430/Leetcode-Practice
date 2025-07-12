0fd00f8db134568bea2fe25e8058c864d0567ba3
WilfredEE <wendeyang2@gmail.com>
2025-07-12 11:40:14 +0800

    add No.206

---
+++ Easy/206.reverse-linked-list.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+
+// Definition for singly-linked list.
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr){} * ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (!head)
+            return nullptr;
+
+        ListNode *node = head;
+        stack<int> tmp;
+        while (node)
+        {
+            tmp.push(node->val);
+            node = node->next;
+        }
+        ListNode *head2 = new ListNode(tmp.top(), nullptr);
+        tmp.pop();
+        ListNode *node2 = head2;
+        while (!tmp.empty())
+        {
+            node2->next = new ListNode(tmp.top(), nullptr);
+            tmp.pop();
+            node2 = node2->next;
+        }
+
+        return head2;
+    }
+};
+// @lc code=end
