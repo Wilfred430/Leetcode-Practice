b2d60da133289d767c0a761b35361baec7868835
WilfredEE <wendeyang2@gmail.com>
2025-07-14 10:54:16 +0800

    add No.50

---
+++ Medium/50.pow-x-n.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=50 lang=cpp
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x,n);
+    }
+};
+// @lc code=end
+
