f47b5adfb31bcafc2a28a1902c0435142843330a
WilfredEE <wendeyang2@gmail.com>
2025-07-14 14:20:55 +0800

    add No.231

--- Easy/231.power-of-two.cpp
+++ Easy/231.power-of-two.cpp
@@ -5,11 +5,23 @@
  */
 
 // @lc code=start
-class Solution {
+#include <iostream>
+using namespace std;
+
+class Solution
+{
 public:
-    bool isPowerOfTwo(int n) {
-        
+    bool isPowerOfTwo(int n)
+    {
+        if (n <= 0)
+            return false;
+        while (n != 1)
+        {
+            if (n % 2 == 1)
+                return false;
+            n /= 2;
+        }
+        return true;
     }
 };
 // @lc code=end
-
