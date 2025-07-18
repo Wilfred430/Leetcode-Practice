99b607c6e1b4fc0459dc996bfdeee29e0b671fc8
WilfredEE <wendeyang2@gmail.com>
2025-07-18 17:26:03 +0800

    modify No.541

--- Easy/541.reverse-string-ii.cpp
+++ Easy/541.reverse-string-ii.cpp
@@ -13,22 +13,36 @@ using namespace std;
 class Solution
 {
 public:
-    string reverseStr(string s, int k)
-    {
-        string result = "";
-        int index;
-        int len = s.length();
-        for (index = 0; index * k < len; ++index)
-        {
-            string tmp = s.substr(index * k, k);
-            if (index % 2 == 0)
-            {
-                reverse(tmp.begin(), tmp.end());
-            }
-            result = result + tmp;
-        }
+    // string reverseStr(string s, int k)
+    // {
+    //     string result = "";
+    //     int index;
+    //     int len = s.length();
+    //     for (index = 0; index * k < len; ++index)
+    //     {
+    //         string tmp = s.substr(index * k, k);
+    //         if (index % 2 == 0)
+    //         {
+    //             reverse(tmp.begin(), tmp.end());
+    //         }
+    //         result = result + tmp;
+    //     }
 
-        return result;
+    //     return result;
+    // }
+    string reverseStr(string &s, int k)
+    {
+        int n = s.size();
+        for (int start = 0; start < n; start += 2 * k)
+        {
+            int i = start;
+            int j = min(start + k - 1, n - 1); // 不超出範圍
+            while (i < j)
+            {
+                swap(s[i++], s[j--]);
+            }
+        }
+        return s;
     }
 };
 // @lc code=end
