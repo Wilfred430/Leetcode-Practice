833a24c39cf609c305c735306db695d2b864b98d
WilfredEE <wendeyang2@gmail.com>
2025-07-27 17:43:45 +0800

    modify No.118

--- Easy/118.pascals-triangle.cpp
+++ Easy/118.pascals-triangle.cpp
@@ -12,34 +12,47 @@ using namespace std;
 class Solution
 {
 public:
+    // vector<vector<int>> generate(int numRows)
+    // {
+    //     if (numRows == 1)
+    //     {
+    //         return {{1}};
+    //     }
+    //     else if (numRows == 2)
+    //    {
+    //         return {{1}, {1, 1}};
+    //     }
+
+    //     vector<vector<int>> result = {{1}, {1, 1}};
+
+    //     for (int i = 3; i <= numRows; ++i)
+    //     {
+    //         vector<int> tmp(i, 0);
+    //         tmp[0] = 1;
+    //         tmp[i - 1] = 1;
+
+    //         int len = result.size() - 1;
+    //         for (int j = 1; j <= i - 2; ++j)
+    //         {
+    //             tmp[j] = result[len][j - 1] + result[len][j];
+    //         }
+
+    //         result.push_back(tmp);
+    //     }
+
+    //     return result;
+    // }
     vector<vector<int>> generate(int numRows)
     {
-        if (numRows == 1)
+        vector<vector<int>> result;
+        for (int i = 0; i < numRows; ++i)
         {
-            return {{1}};
-        }
-        else if (numRows == 2)
-        {
-            return {{1}, {1, 1}};
-        }
-
-        vector<vector<int>> result = {{1}, {1, 1}};
-
-        for (int i = 3; i <= numRows; ++i)
-        {
-            vector<int> tmp(i, 0);
-            tmp[0] = 1;
-            tmp[i - 1] = 1;
-
-            int len = result.size() - 1;
-            for (int j = 1; j <= i - 2; ++j)
+            result.push_back(vector<int>(i + 1, 1)); // 每行首尾都預設1
+            for (int j = 1; j < i; ++j)
             {
-                tmp[j] = result[len][j - 1] + result[len][j];
+                result[i][j] = result[i - 1][j - 1] + result[i - 1][j]; // 根據上一行計算
             }
-
-            result.push_back(tmp);
         }
-
         return result;
     }
 };
