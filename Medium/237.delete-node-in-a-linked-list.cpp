96806310fc4ba41022f306cc025acbb48ff29a2d
WilfredEE <wendeyang2@gmail.com>
2025-07-28 14:18:58 +0800

    add No.237

---
+++ Medium/237.delete-node-in-a-linked-list.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=237 lang=cpp
+ *
+ * [237] Delete Node in a Linked List
+ */
+
+// @lc code=start
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+// @lc code=end
