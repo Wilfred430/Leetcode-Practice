c03646ec98c427390d6286a8652fd52773aa3ab5
WilfredEE <wendeyang2@gmail.com>
2025-08-03 22:39:45 +0800

    add chain No.119

--- Easy/119.pascals-triangle-ii.cpp
+++ Easy/119.pascals-triangle-ii.cpp
@@ -12,32 +12,44 @@ using namespace std;
 class Solution
 {
 public:
+    // vector<vector<int>> Answer;
 
-    vector<vector<int>> Answer;
+    // Solution()
+    // {
+    //     this->Answer.push_back({1});
+    //     this->Answer.push_back({1,1});
 
-    Solution()
-    {
-        this->Answer.push_back({1});
-        this->Answer.push_back({1,1});
+    //     for(int i=2;i<=33;++i)
+    //     {
+    //         vector<int> result(i+1,0);
+    //         result[0] = 1;
+    //         result[i] = 1;
+    //         for(int j = 1;j<i;++j)
+    //         {
+    //             result[j] = this->Answer[i-1][j] + this->Answer[i-1][j-1];
+    //         }
 
-        for(int i=2;i<=33;++i)
-        {
-            vector<int> result(i+1,0);
-            result[0] = 1;
-            result[i] = 1;
-            for(int j = 1;j<i;++j)
-            {
-                result[j] = this->Answer[i-1][j] + this->Answer[i-1][j-1];
-            }
+    //         this->Answer.push_back(result);
+    //     }
+    // }
 
-            this->Answer.push_back(result);
-        }
-    }
+    // vector<int> getRow(int rowIndex)
+    // {
+    //     return this->Answer[rowIndex];
+    // }
 
     vector<int> getRow(int rowIndex)
     {
-        return this->Answer[rowIndex];
+
+        vector<int> row(rowIndex + 1);
+        row[0] = 1;
+
+        for (int i = 1; i <= rowIndex; ++i)
+        {
+            row[i] = (long long)row[i - 1] * (rowIndex - i + 1) / i;
+        }
+
+        return row;
     }
-    
 };
 // @lc code=end
