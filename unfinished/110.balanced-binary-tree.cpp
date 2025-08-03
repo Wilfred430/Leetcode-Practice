bb2af36a4bde2689507aa4be8afacc1e0afea3b5
WilfredEE <wendeyang2@gmail.com>
2025-08-03 22:10:06 +0800

    add No.110

---
+++ unfinished/110.balanced-binary-tree.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=110 lang=cpp
+ *
+ * [110] Balanced Binary Tree
+ */
+
+// @lc code=start
+#include<iostream>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    bool isBalanced(TreeNode *root)
+    {
+        return Answer(root);
+    }
+
+    int Answer(TreeNode* root)
+    {
+
+    }
+};
+// @lc code=end
