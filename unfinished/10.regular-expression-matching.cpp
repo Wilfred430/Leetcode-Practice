0208ea37c7ccd087fc23d30b846d605607726d8a
WilfredEE <wendeyang2@gmail.com>
2025-08-03 21:41:26 +0800

    add none finish N0.10

---
+++ unfinished/10.regular-expression-matching.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=10 lang=cpp
+ *
+ * [10] Regular Expression Matching
+ */
+
+// @lc code=start
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isMatch(string s, string p)
+    {
+    }
+};
+// @lc code=end
---
+++ unfinished/331.verify-preorder-serialization-of-a-binary-tree.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=331 lang=cpp
+ *
+ * [331] Verify Preorder Serialization of a Binary Tree
+ */
+
+// @lc code=start
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValidSerialization(string preorder)
+    {
+    }
+};
+// @lc code=end
