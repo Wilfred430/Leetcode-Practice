22ecd9bfca03a5915340b4460b2b8e9d80682eca
WilfredEE <wendeyang2@gmail.com>
2025-08-04 19:37:45 +0800

    add No.205

---
+++ Easy/205.isomorphic-strings.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=205 lang=cpp
+ *
+ * [205] Isomorphic Strings
+ */
+
+// @lc code=start
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isIsomorphic(string s, string t)
+    {
+
+        if (s.length() != t.length())
+            return false;
+
+        unordered_map<char, char> mapST;
+        unordered_map<char, char> mapTS;
+
+        for (int i = 0; i < s.length(); ++i)
+        {
+            char c1 = s[i], c2 = t[i];
+
+            if (mapST.count(c1))
+            {
+                if (mapST[c1] != c2)
+                    return false;
+            }
+            else
+            {
+                mapST[c1] = c2;
+            }
+
+            if (mapTS.count(c2))
+            {
+                if (mapTS[c2] != c1)
+                    return false;
+            }
+            else
+            {
+                mapTS[c2] = c1;
+            }
+        }
+
+        return true;
+    }
+};
+// @lc code=end
